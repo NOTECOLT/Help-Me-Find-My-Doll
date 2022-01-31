e900c95197b6dd30cb15da0bca0981d170897124
SleepyFeeshy <zromualdez@gmail.com>
2022-01-31 13:08:44 +0800

    Import from other project
    Import existing work

---
+++ Project Doll/Assets/Scripts/FOV.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+    // Config params
+    [SerializeField] private LayerMask layerMask;
+
+    [Header("FOV Settings")]
+    [SerializeField] float fov = 90f;
+    [SerializeField] int rayCount = 2;
+    [SerializeField] float viewAngleSetting = 0f;
+    [SerializeField] float viewDistance = 10f;
+
+    // Variables
+    Vector3 origin;
+    float angleIncrease;
+    float angle;
+
+    // Cached references
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        angleIncrease = fov / rayCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateFOV();
+    }
+
+    private void CreateFOV()
+    {
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+        angle = viewAngleSetting;
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+
+            if (raycastHit2D.collider == null)
+            {
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                vertex = raycastHit2D.point;
+            }
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f); ;
+    }
+
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetDirection(Vector3 aimDirection)
+    {
+        viewAngleSetting = GetAngleFromVector(aimDirection) - fov/2f;
+    }
+
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    public static float GetAngleFromVector(Vector3 vector)
+    {
+        vector = vector.normalized;
+        float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+        if(angle < 0)
+        {
+            angle += 360;
+        }
+        return Mathf.RoundToInt(angle);
+    }
+}
---
+++ Project Doll/Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Config params
+    [SerializeField] float playerMovespeed = 100f;
+    [SerializeField] float interactDistance = 10f;
+    [SerializeField] LayerMask layerMaskInteractables;
+    // Cached references
+    FOV fov;
+
+    // Variables
+    float[] directionFacing;
+    RaycastHit2D raycastHitInteractables;
+    float deltaX;
+    float deltaY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fov = FindObjectOfType<FOV>();
+        directionFacing = new float[2];
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        FindInteractables();
+    }
+
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Moves player
+        deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerMovespeed;
+        deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerMovespeed;
+        SetPlayerFOV(deltaX, deltaY);
+        GetComponent<Rigidbody2D>().position += new Vector2(deltaX, deltaY);
+        //transform.position = new Vector2(transform.position.x + deltaX, transform.position.y + deltaY);
+    }
+
+    private Vector2 GetPlayerDirection()
+    {
+        
+        if (Input.GetAxis("Horizontal") >= 0.1 || Input.GetAxis("Horizontal") <= -0.1)
+        {
+            directionFacing[0] = Mathf.Sign(Input.GetAxis("Horizontal"))*1;
+            directionFacing[1] = 0;
+        }
+        if (Input.GetAxis("Vertical") >= 0.1 || Input.GetAxis("Vertical") <= -0.1)
+        {
+            directionFacing[1] = Mathf.Sign(Input.GetAxis("Vertical"))*1;
+            directionFacing[0] = 0;
+        }
+        Vector2 directionVector = new Vector2(directionFacing[0], directionFacing[1]);
+        //Debug.Log(GetAngleFromVector(directionVector));
+        return directionVector;
+        //Debug.Log(directionFacing[0]);
+    }
+
+    private void FindInteractables()
+    {
+        // Uses raycast2D to check if there are interactables that can be interacted with
+        Debug.DrawRay(transform.position, GetPlayerDirection(), Color.white, 0.01f);
+        raycastHitInteractables = Physics2D.Raycast(transform.position, GetPlayerDirection(), interactDistance, layerMaskInteractables);
+        if (raycastHitInteractables)
+        {
+            raycastHitInteractables.collider.SendMessage("ShowText");
+            Debug.Log(raycastHitInteractables.collider.name);
+        }
+    }
+
+    private void SetPlayerFOV(float deltaX, float deltaY)
+    {
+        fov.SetOrigin(transform.position);
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            // yes
+        }
+        else
+        {
+            fov.SetDirection(new Vector3(-deltaY, deltaX));
+        }
+    }
+
+    public static float GetAngleFromVector(Vector2 vector)
+    {
+        float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+        return Mathf.RoundToInt(angle);
+    }
+}
