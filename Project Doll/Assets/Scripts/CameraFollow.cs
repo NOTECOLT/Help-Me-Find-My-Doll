e2387e4a5ea1e252db8861efa1d2b9ce6d1df2c5
SleepyFeeshy <zromualdez@gmail.com>
2022-01-31 13:12:27 +0800

    Camera script rename to CameraFollow
    Rename camera script to CameraFollow

--- Project Doll/Assets/Scripts/CameraFollow.cs
+++ Project Doll/Assets/Scripts/CameraFollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     [SerializeField] GameObject cameraTarget;
     // Start is called before the first frame update
