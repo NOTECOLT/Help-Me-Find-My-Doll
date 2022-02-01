9acbb3f1531d96aff064beb3e813dff35fd8b533
SleepyFeeshy <zromualdez@gmail.com>
2022-02-01 13:27:15 +0800

    Update TimeManager.cs
    Moved some TimeManager.cs functionality to GameSessionManager

--- Project Doll/Assets/Scripts/TimeManager.cs
+++ Project Doll/Assets/Scripts/TimeManager.cs
@@ -7,7 +7,6 @@ public class TimeManager : MonoBehaviour
     // Configuration parameters
     [SerializeField] float timeLimit = 30f;
     [SerializeField] [Range(0, 20)] float timeScale = 1f;
-    [SerializeField] bool allowGameEnd = false;
 
     // Variables
     [SerializeField] float timeRemaining; // serialized for debug purposes
@@ -15,34 +14,28 @@ public class TimeManager : MonoBehaviour
 
     // Cached references
     GameSessionManager gameSessionManager;
-    SceneLoader sceneLoader;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         timeRemaining = timeLimit;
-        gameSessionManager = FindObjectOfType<GameSessionManager>();
-        sceneLoader = FindObjectOfType<SceneLoader>();
+        gameSessionManager = FindObjectOfType<GameSessionManager>();      
     }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = timeScale;
-        OnTimerEnd();
+        CalculateTimeRemaining();
     }
 
-    private void OnTimerEnd()
+    private void CalculateTimeRemaining()
     {
         // variables on time
         timeElapsedPercentage = (1 - (timeRemaining / timeLimit)) * 100;
         //Debug.Log("Time percentage: " + timeElapsedPercentage);
         timeRemaining -= Time.deltaTime;
-
-        if (timeRemaining <= 0 && allowGameEnd)
-        {
-            sceneLoader.LoadGameOverScene();
-        }
     }
 
     public float GetTimeElapsedPercentage()
