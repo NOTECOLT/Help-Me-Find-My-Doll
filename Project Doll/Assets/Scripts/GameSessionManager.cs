b10f886c264806036ebc62ee72ae449087124242
SleepyFeeshy <zromualdez@gmail.com>
2022-02-01 13:26:03 +0800

    Update GameSessionManager.cs
    Added timer functionality with UI text

--- Project Doll/Assets/Scripts/GameSessionManager.cs
+++ Project Doll/Assets/Scripts/GameSessionManager.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSessionManager : MonoBehaviour
 {
     [SerializeField] float[] timePeriodsByPercentage;
     [SerializeField] FOVProfiles[] fovProfiles;
+    [SerializeField] Text timeText;
+    [SerializeField] bool allowGameEnd = false;
 
     // Cached references
     TimeManager timeManager;
     FOV fov;
+    SceneLoader sceneLoader;
+
     // variables
     int timeIndex;
 
     // Start is called before the first frame update
     void Start()
     {
+        sceneLoader = FindObjectOfType<SceneLoader>();
         timeManager = FindObjectOfType<TimeManager>();
         fov = FindObjectOfType<FOV>();
         fov.SetFOVSettings(fovProfiles[0].GetFOV(), fovProfiles[0].GetViewDistance());
@@ -25,6 +31,13 @@ public class GameSessionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CycleThroughFOVProfile();
+        SetTimeText();
+        End();
+    }
+
+    private void CycleThroughFOVProfile()
+    {
         if (timeIndex < (timePeriodsByPercentage.Length))
         {
             if ((timeManager.GetTimeElapsedPercentage() >= timePeriodsByPercentage[timeIndex]))
@@ -33,6 +46,18 @@ public class GameSessionManager : MonoBehaviour
                 timeIndex++;
             }
         }
-        
+    }
+
+    private void SetTimeText()
+    {
+        timeText.text = Mathf.RoundToInt(timeManager.GetTimeElapsedPercentage()).ToString() + "%";
+    }
+
+    private void End()
+    {
+        if (timeManager.GetTimeElapsedPercentage() >= 100 && allowGameEnd)
+        {
+            sceneLoader.LoadGameOverScene();
+        }
     }
 }
