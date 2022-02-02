f6a549d756ceddfcb8fe6d40a03a11393d238516
SleepyFeeshy <zromualdez@gmail.com>
2022-02-02 12:02:11 +0800

    Night 1 scene
    Started Night 1 scene

---
+++ Project Doll/Assets/Scripts/InteractableObjects/Bed.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void EndNight()
+    {
+        if (EventFlagManager.Instance.GetFlagValue("dollCollected"))
+        {
+            Debug.Log("Epic");
+        }
+        else
+        {
+            Debug.Log(EventFlagManager.Instance.GetFlagValue("dollCollected"));
+            Debug.Log("No doll");
+        }
+        //EventFlagManager.Instance.FlagTickTrue("chairDrag");
+    }
+}
