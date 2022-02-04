b7946005f88c1d55b026fb55aa4c5f6bd7578c82
notecolt <d27thColt@gmail.com>
2022-02-04 23:00:23 +0800

    Squashed commit of the following:
    commit f5fe84d1f1efd54ad004b12d9d2a1b1d2f1e560d
    Author: SleepyFeeshy <zromualdez@gmail.com>
    Date:   Fri Feb 4 19:11:27 2022 +0800
    
        Created Title Screen and added buttons to Game Over screen
    
    commit 6ed10a328d93a7f3f535a1696b4602c55e5ceef6
    Author: SleepyFeeshy <zromualdez@gmail.com>
    Date:   Fri Feb 4 18:47:59 2022 +0800
    
        SceneLoader more functions
    
        LoadNextScene, LoadMainMenu, QuitGame

--- Project Doll/Assets/Scripts/SceneLoader.cs
+++ Project Doll/Assets/Scripts/SceneLoader.cs
@@ -9,6 +9,25 @@ public class SceneLoader : MonoBehaviour
     public void LoadGameOverScene()
     {
         // Loads game over scene using build index
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
+
+    public void LoadNextScene()
+    {
+        // Loads next sceme
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMainMenu()
+    {
+        // Loads next sceme
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        // Loads next sceme
+        Application.Quit();
+    }
+
 }
